c0a178b8e0b3e49124f48dbb5e444847375b5dd2
Ana Júlia Souza <anajuliapereiradesouza003@gmail.com>
2026-01-28 21:25:33 -0400

    feat: add reminders panel and shopping list panel to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Header from "../components/header";
+import Header from "../components/header/header";
+import { RemindersPanel } from "../components/reminders/reminders-panel";
+import { ShoppingListPanel } from "../components/shopping-list/shopping-list-panel";
 
 export default function Page() {
   return (
     <div>
       <Header />
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 sm:gap-6 sm:max-w-4xl  px-10 mx-auto mb-10">
+        <RemindersPanel />
+        <ShoppingListPanel />
+      </div>
     </div>
   );
 }
